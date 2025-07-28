74c29b84bb7c9a252dc516a3c99cc889531f37b3
Edhiel <150269404+Edhiel@users.noreply.github.com>
2025-07-28 22:38:31 +0800

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // <-- 1. Tambahan baru
 
 // Konfigurasi Firebase dari Anda
 const firebaseConfig = {
@@ -8,7 +9,7 @@ const firebaseConfig = {
   authDomain: "managementstokgudang.firebaseapp.com",
   databaseURL: "https://managementstokgudang-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "managementstokgudang",
-  storageBucket: "managementstokgudang.firebasestorage.app",
+  storageBucket: "managementstokgudang.appspot.com",
   messagingSenderId: "619194018490",
   appId: "1:619194018490:web:43f0480613276521c57083"
 };
@@ -17,5 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Ekspor service yang akan kita gunakan di komponen lain
-export const db = getDatabase(app);
+export const db = getDatabase(app); // Ini untuk Realtime Database (lama)
 export const auth = getAuth(app);
+export const firestoreDb = getFirestore(app); // <-- 2. Tambahan baru untuk Firestore
